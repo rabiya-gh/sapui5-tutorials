d539a2c84448f6efe97fd6acdb043ee99b315f67
Rabiya <rabiya.snetonly@gmail.com>
2025-01-16 07:00:29 +0000

    Initial commit: Added SAPUI5 project file

---
+++ ui5_tutorial/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/ui5tutorial/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ui5_tutorial/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/ui5_tutorial/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
